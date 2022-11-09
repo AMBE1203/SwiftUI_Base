abc6fbe633b2a2211dc3e9cb72f0dd95ab596284
cuongdt <3pv@*f#FDb6zzp+>
2022-11-09 17:36:07 +0700

    Initial Commit

---
+++ IosTakeHomeTest/IosTakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosTakeHomeTestApp.swift
+//  IosTakeHomeTest
+//
+//  Created by AMBE on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct IosTakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
